ff94caef3fcba22ff4617345f8f7f0659ea83676
Moises-Bruce <contato.moisesbrucearaujo@gmail.com>
2022-08-12 23:28:18 -0700

    finalizado header e conteúdo do post

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Header } from "./components/Header"
-import { Post } from "./Post"
+import { Post } from "./components/Post"
+import { Sidebar } from "./components/Sidebar";
 
 import styles from "./App.module.css";
 
 import './global.css'
-import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
@@ -14,15 +14,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-        <Post 
-          author="Moisés Bruce" 
-          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus, inventore vel voluptas impedit ducimus nemo quis recusandae corrupti illum possimus maxime ipsa aspernatur tenetur sed ad repellat officia corporis! Tempore."
-        />
-
-        <Post 
-          author="Gabriella Fraiji"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum officiis vero, placeat eum dolorum magni debitis est dolorem fugiat quo quos officia fugit laudantium maiores et nesciunt rem id totam!"
-        />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
