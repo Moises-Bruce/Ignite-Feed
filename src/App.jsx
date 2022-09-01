c2b81f3d0e06bd17f575e37c4f429dc91d1d7d1b
Moises-Bruce <contato.moisesbrucearaujo@gmail.com>
2022-09-01 14:49:55 -0700

    inserindo comentários

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const posts = [
     content: [
         { type: 'paragraph', content: 'Fala galeraa 👋' },
         { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-        { type: 'link', content: '<a href="#">jane.design/doctorcare</a>' } ,
+        { type: 'link', content: 'jane.design/doctorcare' } ,
     ],
     publishedAt: new Date('2022-08-23 20:00:00'),
   },
@@ -31,7 +31,7 @@ const posts = [
     content: [
         { type: 'paragraph', content: 'Fala galeraa 👋' },
         { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-        { type: 'link', content: '<a href="#">jane.design/doctorcare</a>' } ,
+        { type: 'link', content: 'jane.design/doctorcare' } ,
     ],
     publishedAt: new Date('2022-08-30 20:00:00'),
   }
