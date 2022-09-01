098991736796025a1d6a168d05f601b5df2bbecf
Moises-Bruce <contato.moisesbrucearaujo@gmail.com>
2022-09-01 16:18:10 -0700

    removendo comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,7 +9,7 @@ import styles from './Post.module.css'
 
 export function Post({ author, publishedAt, content }){
     const [comments, setComments] = useState([
-        'Post muito bacana!',
+        'Post muito bacana!'
         
     ]);
 
@@ -35,6 +35,16 @@ export function Post({ author, publishedAt, content }){
         setNewCommentText(event.target.value);
     }
 
+    function deleteComment(commentToDelete){
+        //imutabilidade -> as variáveis não sofrem mutação
+        
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        })
+
+        setComments(commentsWithoutDeletedOne);
+    }
+
     return(
         <article className={styles.post}>
             <header>
@@ -54,9 +64,9 @@ export function Post({ author, publishedAt, content }){
             <div className={styles.content}>
                {content.map(line => {
                 if(line.type === 'paragraph'){
-                    return <p>{line.content}</p>;
+                    return <p key={line.content}>{line.content}</p>;
                 }else if(line.type === 'link'){
-                    return <p><a href="#">{line.content}</a></p>;
+                    return <p key={line.content}><a href="#">{line.content}</a></p>;
                 }
                })}
             </div>
@@ -77,7 +87,13 @@ export function Post({ author, publishedAt, content }){
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment content={comment}/>
+                    return (
+                        <Comment 
+                            key={comment} 
+                            content={comment} 
+                            onDeleteComment={deleteComment} 
+                        />
+                    )
                 })}
             </div>
         </article>
